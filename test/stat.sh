00ade1c4847e7cf9223b47afe6af61a5102ad8d3
Sergey Khokhlov <32588378+Hohlas@users.noreply.github.com>
2024-12-06 11:55:34 +0300

    Update stat.sh
    усреднение за 5сек
    iostat -d 5 2

--- test/stat.sh
+++ test/stat.sh
@@ -3,10 +3,10 @@
  echo "  - iostat.v2 second request -"
 
 # Получаем вывод команды iostat -d для nvme устройств
-output=$(iostat -d 1 2 | awk '/^Device/{count++; if (count == 2) {found=1; next}} found && /^nvme/{print $1, $2, $3, $4}')
+output=$(iostat -d 5 2 | awk '/^Device/{count++; if (count == 2) {found=1; next}} found && /^nvme/{print $1, $2, $3, $4}')
 
 # Получаем вывод команды iostat -x для %util
-util_output=$(iostat -x 1 2 | awk '/^Device/{count++; if (count == 2) {found=1; next}} found && /^nvme/ {print $1, $23}') # $23 соответствует %util
+util_output=$(iostat -x 5 2 | awk '/^Device/{count++; if (count == 2) {found=1; next}} found && /^nvme/ {print $1, $23}') # $23 соответствует %util
 
 # Выводим заголовок таблицы
 printf "%-15s %-10s %-12s %-12s %-12s %-12s\n" "Device" "tps" "kB_read/s" "kB_wrtn/s" "tps*(r+w)" "%util"
